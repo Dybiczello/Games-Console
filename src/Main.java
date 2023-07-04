70f968e9e5e63f441dc67424a70f4ceb5e902a39
Mateusz Dybicz <01169274@pw.edu.pl>
2023-07-04 12:25:57 +0200

    Brick breaker pierwsza latwy poziom

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame obj = new JFrame();
+        GamePlay gamePlay = new GamePlay();
+        obj.setBounds(10,10,710,600);
+        obj.setTitle("Brick Breaker");
+        obj.setResizable(false);
+        obj.setVisible(true);
+        obj.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        obj.add(gamePlay);
+    }
+}
---
+++ src/MapGenerator.java
@@ -0,0 +1,53 @@
+import java.awt.*;
+
+import static java.lang.Math.random;
+
+public class MapGenerator {
+    public int map[][];
+    public int brickWidht;
+    public int brickHeight;
+    public MapGenerator(int row, int col){
+        map = new int[row][col];
+        for(int i=0; i < map.length; i++){
+            for(int j=0; j< map[0].length; j++){
+                map[i][j] = (int)(Math.random() * 3 + 1);
+            }
+        }
+        brickWidht = 540/col;
+        brickHeight = 150/row;
+    }
+    public void draw(Graphics2D g) {
+        for(int i=0; i < map.length; i++){
+            for(int j=0; j< map[0].length; j++){
+                if (map[i][j] == 1){
+                    g.setColor(Color.CYAN);
+                    g.fillRect(j * brickWidht + 80, i * brickHeight + 50, brickWidht, brickHeight);
+
+                    g.setStroke(new BasicStroke(3));
+                    g.setColor(Color.BLACK);
+                    g.drawRect(j * brickWidht + 80, i * brickHeight + 50, brickWidht, brickHeight);
+                }
+                if (map[i][j] == 2){
+                    g.setColor(Color.GREEN);
+                    g.fillRect(j * brickWidht + 80, i * brickHeight + 50, brickWidht, brickHeight);
+
+                    g.setStroke(new BasicStroke(3));
+                    g.setColor(Color.BLACK);
+                    g.drawRect(j * brickWidht + 80, i * brickHeight + 50, brickWidht, brickHeight);
+                }
+                if (map[i][j] == 3){
+                    g.setColor(Color.RED);
+                    g.fillRect(j * brickWidht + 80, i * brickHeight + 50, brickWidht, brickHeight);
+
+                    g.setStroke(new BasicStroke(3));
+                    g.setColor(Color.BLACK);
+                    g.drawRect(j * brickWidht + 80, i * brickHeight + 50, brickWidht, brickHeight);
+                }
+            }
+        }
+    }
+    public void setBrickValue(int value, int row, int col){
+        map[row][col] = value;
+    }
+
+}
